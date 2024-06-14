449c911fa9e582763b88ccbaa69fbf3ed387e0f4
stephendanielxxx <stephendanielxxx@gmail.com>
2024-06-14 09:13:22 +0700

    day 5

--- app/src/main/java/app/training/androidtraininginhouse/retrofit/NewsApiService.kt
+++ app/src/main/java/app/training/androidtraininginhouse/retrofit/NewsApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsApiService {
-    @GET("top-headlines/{account}")
+    @GET("top-headlines")
     suspend fun getNews(@Query("country") country: String,
                         @Query("apiKey") apiKey: String,
                         @Query("category") category: String): Response<NewsResponse>
